0a2b8976033a309ecf5299c163e11501e2260808
Bradley Golden <golden.bradley@gmail.com>
2025-12-31 01:24:46 -0600

    Add new/1 factory function to ExecResult for API consistency
    Adds documentation and a factory function to ExecResult matching the
    pattern used by other structs like Instance and Response.

--- lib/strider/sandbox/exec_result.ex
+++ lib/strider/sandbox/exec_result.ex
@@ -1,6 +1,21 @@
 defmodule Strider.Sandbox.ExecResult do
   @moduledoc """
   Struct representing the result of executing a command in a sandbox.
+
+  ## Fields
+
+  - `stdout` - Standard output from the command
+  - `stderr` - Standard error from the command
+  - `exit_code` - Exit code of the command (0 typically means success)
+
+  ## Examples
+
+      %Strider.Sandbox.ExecResult{
+        stdout: "hello world",
+        stderr: "",
+        exit_code: 0
+      }
+
   """
 
   @type t :: %__MODULE__{
@@ -10,4 +25,18 @@ defmodule Strider.Sandbox.ExecResult do
         }
 
   defstruct stdout: "", stderr: "", exit_code: 0
+
+  @doc """
+  Creates a new ExecResult with the given attributes.
+
+  ## Examples
+
+      iex> Strider.Sandbox.ExecResult.new(stdout: "hello", exit_code: 0)
+      %Strider.Sandbox.ExecResult{stdout: "hello", stderr: "", exit_code: 0}
+
+  """
+  @spec new(keyword()) :: t()
+  def new(attrs \\ []) do
+    struct(__MODULE__, attrs)
+  end
 end
